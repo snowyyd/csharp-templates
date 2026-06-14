c962f09f3cf7e7d17b739edd2d63374a3626efd1
Agustín <17956512+snowyyd@users.noreply.github.com>
2026-06-14 10:08:18 -0300

    refactor(bepinex5plugin): initialize Instance property on constructor

--- templates/bepinex5plugin/MyTemplate/Program.cs
+++ templates/bepinex5plugin/MyTemplate/Program.cs
@@ -8,17 +8,18 @@ namespace MyTemplate;
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 internal class Plugin : BaseUnityPlugin
 {
-	internal static Plugin? Instance { get; private set; }
+	internal static Plugin Instance { get; private set; } = null!;
 	internal Harmony HarmonyInstance = null!;
 	internal new ManualLogSource Logger => base.Logger;
 
+	internal Plugin()
+	{
+		Instance = this;
+	}
+
 	private void Awake()
 	{
-		// Plugin startup logic
-		Instance = this;
 		this.Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
-
-		// Harmony patches
 		this.HarmonyInstance = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
 	}
 
@@ -26,6 +27,5 @@ internal class Plugin : BaseUnityPlugin
 	{
 		this.Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID}'s OnDestroy() got called!");
 		this.HarmonyInstance.UnpatchSelf();
-		Instance = null;
 	}
 }
